e7d4bcbf369a339c075dc7dc69aaeb66f45cd9a5
Nicolle Lima <168025081+nicswx@users.noreply.github.com>
2024-07-01 19:44:39 -0300

    finalização heart

--- heart.py
+++ heart.py
@@ -1,11 +1,18 @@
+import math 
 from turtle import *
-color ('red')
-begin_fill()
-pensize(3)
-left(50)
-forward(133)
-circle(50, 200)
-right(140)
-circle(50, 200)
-forward(133)
-end_fill()
+def hearta(k):
+    return 15*math.sin(k)**3
+def heartb(k):
+    return 12*math.cos(k)-5*\
+          math.cos(2*k)-2*\
+math.cos(3*k)-\
+math.cos(4*k)
+
+speed(0)
+bgcolor("pink")
+for i in range(10000):
+    goto(hearta(i)*20, heartb(i)*20)
+for j in range(5):
+    color("black")
+goto(0, 0)
+done()
