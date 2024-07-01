7d4332303440e5698beb9caf1f3942b6016c04fe
Nicolle Lima <168025081+nicswx@users.noreply.github.com>
2024-07-01 19:19:59 -0300

    comando coração 3

--- heart.py
+++ heart.py
@@ -1,53 +1,11 @@
-import turtle 
-
-window = turtle.Screen()
-window.bgcolor("pink")
-window.title("Heart")
-
-pen = turtle.Turtle()
-pen.color("red")
-pen.fillcolor("red")
-pen.pensize(3)
-pen.speed(7)
-
-pen.begin_fill()
-pen.left(140)
-pen.forward(224)
-for _ in range(200):
-    pen.right(1)
-    pen.forward(2)
-pen.left(120)
-for _ in range(200):
-    pen.right(1)
-    pen.forwar(2)
-pen.forward(224)
-pen.end_fill()
-
-
-pen = turtle.Turtle()
-pen.color("red")
-pen.fillcolor("red")
-pen.pensize(3)
-pen.speed(7)
-
-pen.begin_fill()
-pen.left(140)
-pen.forward(224)
-for _ in range(200):
-    pen.right(1)
-    pen.forward(2)
-pen.left(120)
-for _ in range(200):
-    pen.right(1)
-    pen.forwar(2)
-pen.forward(224)
-pen.end_fill()
-
-pen.up()
-pen.goto(0, -70)
-pen.down()
-pen.color("black")
-
-pen.hideturble()
-
-turtle.done()
+from turtle import *
+color ('red')
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle(50, 200)
+right(140)
+circle(50, 200)
+forward(133)
+end_fill()
